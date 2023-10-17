cc52358febfa859e9ac29ebc125c9ac2399582ef
iShantaram <1agapovav987@gmail.com>
2023-10-17 17:27:28 +0300

    В CalcServiceTest созданы тесты для каждого метода. Для plus, minus и multiply использовано по два набора параметров, для divide - три. В CalcServiceParametrizedTest созданы параметризованные тесты для plus, minus, multiply и divide. В CalcService вынесены в константы тексты приветственного сообщения и сообщения об ошибке при делении на 0. CalcService изменен - теперь все функции возвращают результат String-ом.

--- src/main/java/com/example/junitcalc/service/CalcService.java
+++ src/main/java/com/example/junitcalc/service/CalcService.java
@@ -4,25 +4,27 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalcService {
+    public final String greetingMessage = "Добро пожаловать в калькулятор!";
+    public final String divideByZeroMessage = "Divide by 0 exception!";
     public String greeting() {
-        return "Добро пожаловать в калькулятор!";
+        return greetingMessage;
     }
-    public Integer plus(Integer num1, Integer num2) {
-        return num1 + num2;
-    }
-
-    public Integer minus(Integer num1, Integer num2) {
-        return num1 - num2;
+    public String plus(Integer num1, Integer num2) {
+        return String.valueOf(num1 + num2);
     }
 
-    public Integer multiply(Integer num1, Integer num2) {
-        return num1 * num2;
+    public String minus(Integer num1, Integer num2) {
+        return String.valueOf(num1 - num2);
     }
 
-    public Double divide(Integer num1, Integer num2) {
+    public String multiply(Integer num1, Integer num2) {
+        return String.valueOf(num1 * num2);
+    }
+
+    public String divide(Integer num1, Integer num2) {
         if (num2 == 0) {
-            throw new IllegalArgumentException("Divide by 0!");
+            throw new IllegalArgumentException(divideByZeroMessage);
         }
-        return (double) num1 / num2;
+        return String.valueOf((double) num1 / num2);
     }
 }
---
+++ src/test/java/com/example/junitcalc/service/CalcServiceTest.java
@@ -0,0 +1,72 @@
+package com.example.junitcalc.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalcServiceTest {
+    private final int[] nabor1 = {5, 7};
+    private final int[] nabor2 = {999999, 999};
+    private final int[] nabor3 = {999, 0};
+
+    private final CalcService calcService = new CalcService();
+
+    @Test
+    public void greetingTest() {
+        String expectedResult = calcService.greetingMessage;
+        String actualResult = calcService.greeting();
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void plusTest() {
+        String expectedResult1 = String.valueOf(nabor1[0] + nabor1[1]);
+        String actualResult1 = calcService.plus(nabor1[0], nabor1[1]);
+        assertEquals(expectedResult1, actualResult1);
+
+        String expectedResult2 = String.valueOf(nabor2[0] + nabor2[1]);
+        String actualResult2 = calcService.plus(nabor2[0], nabor2[1]);
+        assertEquals(expectedResult2, actualResult2);
+    }
+
+    @Test
+    public void minusTest() {
+        String expectedResult1 = String.valueOf(nabor1[0] - nabor1[1]);
+        String actualResult1 = calcService.minus(nabor1[0], nabor1[1]);
+        assertEquals(expectedResult1, actualResult1);
+
+        String expectedResult2 = String.valueOf(nabor2[0] - nabor2[1]);
+        String actualResult2 = calcService.minus(nabor2[0], nabor2[1]);
+        assertEquals(expectedResult2, actualResult2);
+    }
+
+    @Test
+    public void multiplyTest() {
+        String expectedResult1 = String.valueOf(nabor1[0] * nabor1[1]);
+        String actualResult1 = calcService.multiply(nabor1[0], nabor1[1]);
+        assertEquals(expectedResult1, actualResult1);
+
+        String expectedResult2 = String.valueOf(nabor2[0] * nabor2[1]);
+        String actualResult2 = calcService.multiply(nabor2[0], nabor2[1]);
+        assertEquals(expectedResult2, actualResult2);
+    }
+
+    @Test
+    public void divideTest() {
+        String expectedResult1 = String.valueOf((double) nabor1[0] / nabor1[1]);
+        String actualResult1 = calcService.divide(nabor1[0], nabor1[1]);
+        assertEquals(expectedResult1, actualResult1);
+
+        String expectedResult2 = String.valueOf((double) nabor2[0] / nabor2[1]);
+        String actualResult2 = calcService.divide(nabor2[0], nabor2[1]);
+        assertEquals(expectedResult2, actualResult2);
+
+        String expectedResult3 = calcService.divideByZeroMessage;
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> calcService.divide(nabor3[0], nabor3[1]));
+        assertEquals(expectedResult3, exception.getMessage());
+    }
+}
