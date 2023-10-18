e781866bb827523b1e02b9a4cf3e22d8db7e58f9
iShantaram <1agapovav987@gmail.com>
2023-10-18 16:34:20 +0300

    Программа доработана: проверки разных наборов вынесены в разные тесты; проведен ренейминг методов класса CalcServiceTest.

--- src/test/java/com/example/junitcalc/service/CalcServiceTest.java
+++ src/test/java/com/example/junitcalc/service/CalcServiceTest.java
@@ -9,8 +9,9 @@ public class CalcServiceTest {
     private final String greetingMessage = "Добро пожаловать в калькулятор!";
     private final String divideByZeroMessage = "Divide by 0 exception!";
     private final int[] nabor1 = {5, 7};
-    private final int[] nabor2 = {999999, 999};
+    private final int[] nabor2 = {-99, -9900};
     private final int[] nabor3 = {999, 0};
+    private final int[] nabor4 = {-18, 3};
 
     private final CalcService calcService = new CalcService();
 
@@ -20,48 +21,63 @@ public class CalcServiceTest {
     }
 
     @Test
-    public void plusTest() {
+    public void plus_shouldReturnPositiveNumWhenNumbersArePositive() {
         String expectedResult1 = String.valueOf(nabor1[0] + nabor1[1]);
         String actualResult1 = calcService.plus(nabor1[0], nabor1[1]);
         assertEquals(expectedResult1, actualResult1);
+    }
 
+    @Test
+    public void plus_shouldReturnNegativeNumWhenNumbersAreNegative() {
         String expectedResult2 = String.valueOf(nabor2[0] + nabor2[1]);
         String actualResult2 = calcService.plus(nabor2[0], nabor2[1]);
         assertEquals(expectedResult2, actualResult2);
     }
 
     @Test
-    public void minusTest() {
-        String expectedResult1 = String.valueOf(nabor1[0] - nabor1[1]);
-        String actualResult1 = calcService.minus(nabor1[0], nabor1[1]);
+    public void minus_shouldReturnPositiveWhenNum1BiggerThenNum2() {
+        String expectedResult1 = String.valueOf(nabor2[0] - nabor2[1]);
+        String actualResult1 = calcService.minus(nabor2[0], nabor2[1]);
         assertEquals(expectedResult1, actualResult1);
+    }
 
-        String expectedResult2 = String.valueOf(nabor2[0] - nabor2[1]);
-        String actualResult2 = calcService.minus(nabor2[0], nabor2[1]);
+    @Test
+    public void minus_shouldReturnNegativeWhenNum1SmallerThenNum2() {
+        String expectedResult2 = String.valueOf(nabor1[0] - nabor1[1]);
+        String actualResult2 = calcService.minus(nabor1[0], nabor1[1]);
         assertEquals(expectedResult2, actualResult2);
     }
 
     @Test
-    public void multiplyTest() {
+    public void multiply_shouldReturnPositiveWhenNumbersHaveTheSameSign() {
         String expectedResult1 = String.valueOf(nabor1[0] * nabor1[1]);
         String actualResult1 = calcService.multiply(nabor1[0], nabor1[1]);
         assertEquals(expectedResult1, actualResult1);
+    }
 
-        String expectedResult2 = String.valueOf(nabor2[0] * nabor2[1]);
-        String actualResult2 = calcService.multiply(nabor2[0], nabor2[1]);
+    @Test
+    public void multiply_shouldReturnNegativeWhenNumbersHaveDifferentSigns() {
+        String expectedResult2 = String.valueOf(nabor4[0] * nabor4[1]);
+        String actualResult2 = calcService.multiply(nabor4[0], nabor4[1]);
         assertEquals(expectedResult2, actualResult2);
     }
 
     @Test
-    public void divideTest() {
+    public void divide_shouldReturnPositiveWhenNumbersHaveTheSameSign() {
         String expectedResult1 = String.valueOf((double) nabor1[0] / nabor1[1]);
         String actualResult1 = calcService.divide(nabor1[0], nabor1[1]);
         assertEquals(expectedResult1, actualResult1);
+    }
 
-        String expectedResult2 = String.valueOf((double) nabor2[0] / nabor2[1]);
-        String actualResult2 = calcService.divide(nabor2[0], nabor2[1]);
+    @Test
+    public void divide_shouldReturnNegativeWhenNumbersHaveDifferentSigns() {
+        String expectedResult2 = String.valueOf((double) nabor4[0] / nabor4[1]);
+        String actualResult2 = calcService.divide(nabor4[0], nabor4[1]);
         assertEquals(expectedResult2, actualResult2);
+    }
 
+    @Test
+    public void divide_shouldReturnExceptionWithMessage() {
         Exception exception = assertThrows(
                 IllegalArgumentException.class,
                 () -> calcService.divide(nabor3[0], nabor3[1]));
